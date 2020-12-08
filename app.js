76abcde27a7c89a0df788a72c35fdee6375c58aa
Philari <51737419+Philari@users.noreply.github.com>
2020-12-08 09:05:25 +0300

    lesson2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 6000
+const port = 5000
 const hostname = 'localhost';
 
+//importing routes
+const productRouter = require("./routes/product.js")
+
 app.get('/', (req, res) => {
-  res.send('Hello From The Server')
-})
+  res.json({
+    username: "philari",
+    password: "xxxxx",
+    description: "logins",
+    msg: "Hello from server"
+   })
+});
+
+//middleware
+app.use("/static", express.static("public"));
+app.use("/product", productRouter);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ routes/product.js
@@ -0,0 +1,23 @@
+//const router = require("express").Router();
+var express = require('express')
+var router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.json(
+        {name:"Allproducts",
+        price:"45000ugx",
+        ProductImage:"https://images.unsplash.com/photo-1597169428688-254b4223ba7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+        )
+});
+
+router.get('/trousers',(req,res)=>{
+    res.json(
+        {name:"trousers",
+        price:"45000ugx",
+        ProductImage:"https://images.unsplash.com/photo-1597169428688-254b4223ba7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+        )
+});
+
+module.exports = router
